a3b55a7e3cd77e1ef7288922bdba424fa79bdd66
sisoia ankush <techankush2021@gmail.com>
2026-01-24 11:09:39 +0530

    arithmatic

---
+++ arithmatic.java
@@ -0,0 +1,13 @@
+public class arithmatic {
+    public static void main (String args[]){
+      int x= 68;
+      int  y =56;
+      System.out.println(x+y);
+      System.out.println(x-y);
+      System.out.println(x*y);
+      System.out.println(x/y);
+      System.out.println(x%y);
+
+        
+    }
+}
