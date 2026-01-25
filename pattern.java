c8cb67ebd56d40aef975a5adce69858b123a8a20
sisoia ankush <techankush2021@gmail.com>
2026-01-25 23:39:28 +0530

    pattern.java

---
+++ pattern.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+//Package learning-java;
+
+
+public class pattern {
+    public static void main (String args[]){
+        Scanner sc= new Scanner(System.in);
+   System.out.println("Enter a number");
+   System.out.println(Math.pow(3.14,3.14));
+    }
+   }
+     
+      
+      
+   
