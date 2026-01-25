70b0f1274c4d0b50236c15dc8f08d75e11f1e1a9
sisoia ankush <techankush2021@gmail.com>
2026-01-25 23:38:07 +0530

    variable.java

---
+++ variable.java
@@ -0,0 +1,16 @@
+public class variablebasics{
+    public static void main (String args[]){
+       int x= 5;
+       System.out.println((x));
+       x=4;
+       System.out.println(x);
+        int y = 6;
+        y= y+7;
+        System.out.println(y);
+        System.out.println(x+y);
+        int z= 20;
+        z= z/2;
+        System.out.println(z+x+y);
+        
+    }
+}
