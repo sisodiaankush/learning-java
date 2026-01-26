33b12058df6637164bcfda78369e49f8a126b136
sisoia ankush <techankush2021@gmail.com>
2026-01-26 22:36:36 +0530

    loop question

---
+++ loop.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+
+public class loop {
+    public static void main (String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter a number");
+        int i =0;//initialisation
+        while (i<=10){
+            System.out.println(i);
+            i=i+2;
+        } 
+
+        } 
+        
+        
+        
+        
+    }
