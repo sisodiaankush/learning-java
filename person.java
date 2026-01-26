f12249978443f3f9138262afa89e68665f67b7e1
sisoia ankush <techankush2021@gmail.com>
2026-01-26 22:35:11 +0530

    question of person

---
+++ person.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+class perosn {
+     int age;
+    String name;
+}
+public class person {
+
+    public static void main(String[] args) {
+        person obj = new person();
+         Scanner sc= new Scanner(System.in);
+System.out.println(obj.age);
+System.out.println(obj.name);
+
+
+
+
+    }
+}
