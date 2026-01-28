f0d0f5674e3b1a7685efbdb6bbb5900b05cff5c1
sisoia ankush <techankush2021@gmail.com>
2026-01-28 23:35:00 +0530

    input class

---
+++ input.java
@@ -0,0 +1,135 @@
+
+import java.util.Scanner;
+//Package learning-java;
+
+
+public class input {
+    public static void main (String args[]){
+        Scanner sc= new Scanner(System.in);
+        /*int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a+b;
+        System.out.println(c);
+        int d= a-b;
+        System.out.println(d);
+        int e = a*b;
+        System.out.println(e);*/
+        
+
+
+/*System.out.println("enter name");
+        String name=  sc.nextLine();
+        System.out.println("enter age : ");
+        int age= sc.nextInt();
+        System.out.println("my name is  " + " : " + name +  " " + "my age is " + " : " + age);*/
+
+        //sqaure of number
+
+        /*System.out.println("Enter number");
+        int n =  sc.nextInt();
+        int square = n*n;
+        System.out.println(square); */
+
+        //area of circle
+
+       /* double pi = 3.14;
+        System.out.println("Enter radius");
+        double r=  sc.nextDouble();
+       double area = pi*r*r;
+        System.out.println("area of circle" + " :  " + area); */
+
+//simple interest
+
+/*System.out.println("enter pinciple");
+double p = sc.nextDouble();
+System.out.println("Enter rate");
+double r= sc.nextDouble();
+System.out.println("enter time");
+double t=  sc.nextDouble();
+
+double si= (p*r*t)/100;
+System.out.println("simple interest : " + " :" + si);*/
+ 
+//modulo operator
+
+ /*int a= 7;
+int b= 4;
+System.out.println(a%b);
+System.out.println(a/b);
+double x=  7;
+double z = 7%2;
+System.out.println(z); */
+
+//how to print ascii value 
+//char ch = 'a';
+/*int c= ch;
+System.out.println(c);
+
+//another way
+
+char ch= 'a';
+int y= (int)ch;
+System.out.println(y);
+
+char a= '3';
+int b = (int)a;
+System.out.println(b);
+
+ //another way
+char k = 'A';
+System.out.println(k+0);
+
+char l= '5';
+System.out.println(l+0);
+
+// char ch = '44'  cant store two character
+// another way
+
+char f=  'b';
+System.out.println((f+f)/2);
+
+char d= '5';
+System.out.println(d+d);
+System.out.println((d+d)/2); 
+
+//integer to character
+
+int a= 65;
+char ch = (char)a;
+System.out.println(ch);
+
+int z= 66;
+char h= (char)z;
+System.out.println(h); 
+
+//BODMAS
+ System.out.println(6*2/3);
+System.out.println(5+4*3); 
+
+//INCREAMENT AND DECREAMENT OPERATOR
+
+int x = 5;
+x++;
+++x;
+x--;
+--x;
+System.out.println(x);
+x=x+1;
+System.out.println(x); 
+
+ int y = 6;
+System.out.println(y++);
+System.out.println(y);
+
+int z= 8;
+System.out.println(++z);
+System.out.println(z); */
+
+int x= 10;
+int y = x++;
+System.out.println(x + " " + y);
+
+
+
+    }
+}
