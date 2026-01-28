fd2c38fb8c8c6853d72545d708841501d7ff0ac8
sisoia ankush <techankush2021@gmail.com>
2026-01-28 23:36:19 +0530

    hello

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello {
+    public static void main (String args[]) {
+        System.out.println("hello world");
+        System.out.println(1+2);
+        System.out.println((3*3));
+        System.out.println(("my name is ankush"));
+    }
+}
